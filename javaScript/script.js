742ca035a0373164feb7660399a9977fa0db6472
Abhishek Srivas <abhiautar123@gmail.com>
2020-08-24 16:55:21 +0530

    Added comments,a panel about difficulties and proper indentation

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,22 +1,26 @@
 //Js time :- lets do this.....................(to check how to do comment)
 
-
 function drop(){ // fuction to make the drop fall
+
     var drop_image = document.getElementById("drop");
+
     // to get the x co-ordinate of mouse position
     var x = event.clientX;
     // to get the y-co-ordinate of mouse position
     var y = event.clientY;
-    drop_image.style.top = y+"px";
-    drop_image.style.left=x+"px";
-    console.log(x);
-    console.log(y);
+
+    drop_image.style.top = y + "px";
+    drop_image.style.left= x + "px";
+
+    console.log("value of x:- " + x); //to check the value of x in console
+    console.log("value of y:- " + y); //to check the value of y in console
+
     document.getElementById("drop").style.display = "block  ";
     var pos = y; 
     var id = setInterval(frame,5) // for call frame funcion again and again
     function frame(){
 
-        if(pos == 2000 ){
+        if(pos == 1800 ){
             clearInterval(id);
         }else{
             pos++;
