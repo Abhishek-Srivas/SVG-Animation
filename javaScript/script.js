7e80e8f6159a5f68221c1acda37f8872918d11e3
Abhishek Srivas <abhiautar123@gmail.com>
2020-08-25 13:39:48 +0530

    Whale movement added,banner and gradient background added at landing page,comments for every function,html element added

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,10 +1,12 @@
 //Js time :- lets do this.....................(to check how to do comment)
 
 
-function start(){
-    
-    document.getElementById("container").style.backgroundImage = "url('/Images/Background.jpg')";
+document.getElementById("welcome").innerHTML = "Welcome, press the button to start your jorney";//starting banner text
+
+function start(){   //to start jouney 
+    document.getElementById("container").style.backgroundImage = "url('../Images/Background.jpg')";
     document.getElementById("start").style.display = "none";
+    document.getElementById("welcome").style.display = "none";
     document.getElementById("fairy").style.display = "block";
 }
 
@@ -23,7 +25,7 @@ function drop(){ // fuction to make the drop fall
     console.log("value of x:- " + x); //to check the value of x in console
     console.log("value of y:- " + y); //to check the value of y in console
 
-    document.getElementById("drop").style.display = "block  ";
+    document.getElementById("drop").style.display = "block";
     var pos = y; 
     var id = setInterval(frame,5) // for call frame funcion again and again
     function frame(){
@@ -38,7 +40,7 @@ function drop(){ // fuction to make the drop fall
       
 }
 
-function fairy(){
+function fairy(){ //Fairy movement and alert
     let fairy = document.getElementById("fairy");
     if(fairy.style.top != 5+"vh" ){                //Trying new things with if else
         fairy.style.top = 5 +"vh";
@@ -52,14 +54,36 @@ function fairy(){
 
 }
 
-function realmChange(){
-    document.getElementById("container").style.backgroundImage = "url('/Images/Background2/Background2.jpg')";
+function realmChange(){ //for realm change with the help of portal button
+    document.getElementById("container").style.backgroundImage = "url('../Images/Background2/Background2.jpg')";
     document.getElementById("fairy").style.display = "none";
     document.getElementById("isekai").style.display = "none";
     document.getElementById("nemo").style.display = "block";
-    document.getElementById("g-fish").style.display = "block";
+    document.getElementById("gfish").style.display = "block";
     document.getElementById("whale").style.display = "block";
+}
 
+function whale(){ // function for the movement of whale
+  
+    let whale = document.getElementById("whale");
+    let gfish = document.getElementById("gfish");
+    let id = setInterval(movement,150);
+    let pos = 89;
+    function movement(){
+        if(pos == 0){
+            clearInterval(id);
+        }
+        else{
+            pos--;
+            whale.style.left = pos + "vw";
+            console.log("whale:- " + whale.style.left);  // for checking the value of whale in console
+            console.log("Fish:- " + gfish.style.left);  // for checking the value of fish in console (Error:- Not showing any output)
+            if(whale.style.left == gfish.style.left){
+                gfish.style.display="none";
+            }
+
+        }
+    }
 }
 
 
